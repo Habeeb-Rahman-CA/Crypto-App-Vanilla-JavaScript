cb59edee45abba6710439d3c9e3cdc0ceca5fcbf
HabeebR07 <habeebrahmanca07@gmail.com>
2024-09-14 16:07:14 +0530

    global.js completed

--- global.js
+++ global.js
@@ -5,7 +5,7 @@ const marketCapChangeElement = document.getElementById('marketCapChange')
 const volume = document.getElementById('volume')
 const dominance = document.getElementById('dominance')
 
-//when the HTML content fully reloaded the it fetch the data
+//when the HTML content fully loaded then fetch the data
 document.addEventListener("DOMContentLoaded", () =>{
     fetchGlobal();
 })
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", () =>{
 //Read the data from localstorage
 const getLocalStorageData = (key) =>{
     const storedData = localStorage.getItem(key)
-    if(!storedData) return null
+    if(!storedData) return null //null if there is no data
 
     const parsedData = JSON.parse(storedData)
     const currentTime = Date.now()
@@ -26,18 +26,21 @@ const getLocalStorageData = (key) =>{
     return parsedData.data
 }
 
+//store the data in local storage with a timestamp
 const setLocalStorageData = (key, data) =>{
     const storedData = {
         timestamp: Date.now(),
         data: data
     }
-    localStorage.setItem(key, JSON.stringify(storedData))
+    localStorage.setItem(key, JSON.stringify(storedData)) //save the data in local storage
 }
 
+//fetch the global cryptocurrency data from "CoinGecko API"
 const fetchGlobal = () =>{
-    const localStorageKey = 'Global_Data'
+    const localStorageKey = 'Global_Data' //define the key
     const localData = getLocalStorageData(localStorageKey)
 
+    //if data is found display it, if not fetch new data
     if (localData){
         displayGlobalData(localData)
     } else {
@@ -46,11 +49,11 @@ const fetchGlobal = () =>{
         fetch('https://api.coingecko.com/api/v3/global', options)
             .then(response => response.json())
             .then(data => {
-                const globalData = data.data
+                const globalData = data.data //extract the data from API response
                 displayGlobalData(globalData)
-                setLocalStorageData(localStorageKey, globalData)
+                setLocalStorageData(localStorageKey, globalData) //and save to local storage
             })
-            .catch(error =>{
+            .catch(error =>{ //if any error show not available
                 coinsCount.textContent = 'N/A'
                 exchangeCount.textContent = 'N/A'
                 marketCap.textContent = 'N/A'
@@ -62,24 +65,29 @@ const fetchGlobal = () =>{
     }
 }
 
+//display the data in the HTML element
 const displayGlobalData = (globalData) =>{
     console.log(globalData)
+
+    //coin and exchange count
     coinsCount.textContent = globalData.active_cryptocurrencies || "N/A"
     exchangeCount.textContent = globalData.markets || "N/A"
 
+    //market cap and its change in % if its above 0 then green and below red
     marketCap.textContent = globalData.total_market_cap?.usd ? `$${(globalData.total_market_cap.usd / 1e12).toFixed(3)}T` : "N/A"
     const marketCapChange = globalData.market_cap_change_percentage_24h_usd
 
     if(marketCapChange !== undefined){
-        const changeText = `${marketCapChange.toFixed(1)}%`
+        const changeText = `${marketCapChange.toFixed(1)}%` // "toFixed" used to fix decimal place here it shows only 1 decimal place
         marketCapChangeElement.innerHTML = `${changeText} <i class="${marketCapChange < 0 ? 'red' : 'green'} ri-arrow-${marketCapChange < 0 ? 'down' : 'up'}-s-fill"></i>`
         marketCapChangeElement.style.color = marketCapChange < 0 ? 'red' : 'green'
     } else {
         marketCapChangeElement.textContent = "N/A"
     }
-
+    // "total_volume?usd" this means if there is total volume exist then access to the usd from total volume.
     volume.textContent = globalData.total_volume?.usd ? `$${(globalData.total_volume.usd / 1e9).toFixed(3)}B` : 'N/A'
 
+    //adding BTC and ETH dominance
     const btcDominance = globalData.market_cap_percentage?.btc ? `${globalData.market_cap_percentage.btc.toFixed(1)}%` : 'N/A'
     const ethDominance = globalData.market_cap_percentage?.eth ? `${globalData.market_cap_percentage.eth.toFixed(1)}%` : 'N/A'
 
